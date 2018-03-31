24511982d6bd99ef63859fd5ddf1e5805bf29466
unlimitedbacon <unlimitedbacon@gmail.com>
2018-03-31 10:59:25 -0700

    Added a benchmark

---
+++ benches/cube_benchmark.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate criterion;
+extern crate stl_thumb;
+
+use criterion::Criterion;
+use stl_thumb::Config;
+
+fn cube() {
+    let config = Config {
+        stl_filename: "cube.stl".to_string(),
+        img_filename: "cube.png".to_string()
+    };
+
+    stl_thumb::run(&config)
+        .expect("Error in run function");
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("cube", |b| b.iter(|| cube()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
