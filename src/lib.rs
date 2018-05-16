69e1e183d97256f20750c462381ab49ef0f048db
unlimitedbacon <unlimitedbacon@gmail.com>
2018-05-16 09:10:59 -0700

    Major refactoring and ripping out three-rs to use glium instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 extern crate cgmath;
 extern crate clap;
+#[macro_use]
+extern crate glium;
+extern crate image;
 extern crate mint;
-extern crate stl_io;
-extern crate three;
+
+mod mesh;
 
 use std::error::Error;
 use std::fs::File;
 use clap::{App, Arg};
 use cgmath::Rotation;
-use stl_io::{Triangle, Vertex};
-use three::{Geometry, Object};
+use mesh::Mesh;
 
 const CAM_AZIMUTH_DEG: f32 = -60.0;
 const CAM_ELEVATION_DEG: f32 = 30.0;
@@ -50,148 +52,7 @@ impl Config {
     }
 }
 
-struct BoundingBox {
-    min: cgmath::Point3<f32>,
-    max: cgmath::Point3<f32>,
-}
-
-impl BoundingBox {
-    fn new(vert: &Vertex) -> BoundingBox {
-        BoundingBox {
-            min: cgmath::Point3 {
-                x: vert[0],
-                y: vert[1],
-                z: vert[2],
-            },
-            max: cgmath::Point3 {
-                x: vert[0],
-                y: vert[1],
-                z: vert[2],
-            },
-        }
-    }
-    fn expand(&mut self, vert: &Vertex) {
-        if vert[0] < self.min.x {
-            self.min.x = vert[0];
-        } else if vert[0] > self.max.x {
-            self.max.x = vert[0];
-        }
-        if vert[1] < self.min.y {
-            self.min.y = vert[1];
-        } else if vert[1] > self.max.y {
-            self.max.y = vert[1];
-        }
-        if vert[2] < self.min.z {
-            self.min.z = vert[2];
-        } else if vert[2] > self.max.z {
-            self.max.z = vert[2];
-        }
-    }
-    fn center(&self) -> cgmath::Point3<f32> {
-        cgmath::Point3 {
-            x: (self.min.x + self.max.x) / 2.0,
-            y: (self.min.y + self.max.y) / 2.0,
-            z: (self.min.z + self.max.z) / 2.0,
-        }
-    }
-}
-
-// Calculate surface normal of triangle using cross product
-// TODO: The GPU can probably do this a lot faster than we can.
-// See if there is an option for offloading this.
-fn normal(tri: &Triangle) -> mint::Vector3<f32> {
-    let p1: cgmath::Vector3<f32> = tri.vertices[0].into();
-    let p2: cgmath::Vector3<f32> = tri.vertices[1].into();
-    let p3: cgmath::Vector3<f32> = tri.vertices[2].into();
-    let v = p2 - p1;
-    let w = p3 - p1;
-    let n = v.cross(w);
-    let mag = n.x.abs() + n.y.abs() + n.z.abs();
-    mint::Vector3 {
-        x: n.x / mag,
-        y: n.y / mag,
-        z: n.z / mag,
-    }
-}
-
-fn process_tri(tri: &Triangle, geo: &mut Geometry, bounds: &mut BoundingBox) {
-    for v in tri.vertices.iter() {
-        bounds.expand(&v);
-        // TODO: Should figure out how to do this with into() instead
-        geo.base.vertices.push(mint::Point3 {
-            x: v[0],
-            y: v[1],
-            z: v[2],
-        });
-        //println!("{:?}", v);
-    }
-    // Use normal from STL file if it is provided, otherwise calculate it ourselves
-    let n: mint::Vector3<f32>;
-    if tri.normal == [0.0, 0.0, 0.0] {
-        println!("Calculating surface normal");
-        n = normal(&tri);
-    } else {
-        n = mint::Vector3 {
-            x: tri.normal[0],
-            y: tri.normal[1],
-            z: tri.normal[2],
-        };
-    }
-    //println!("{:?}",tri.normal);
-    for _ in 0..3 {
-        geo.base.normals.push(n);
-    }
-}
-
-fn debug_geo(geometry: &Geometry) {
-    println!("Verts: {}", geometry.base.vertices.len());
-    println!("Norms: {}", geometry.base.normals.len());
-    println!("Tangents: {}", geometry.base.tangents.len());
-    println!("Tex Coords: {:?}", geometry.tex_coords);
-    println!("Faces: {:?}", geometry.faces.len());
-    println!("Joints: {:?}", geometry.joints);
-    println!("Shapes: {:?}", geometry.shapes);
-    println!();
-}
-
-fn load_mesh(mut stl_file: File) -> Result<(Geometry, BoundingBox), Box<Error>> {
-    //let stl = stl_io::read_stl(&mut stl_file)?;
-    //println!("{:?}", stl);
-    let mut stl_iter = stl_io::create_stl_reader(&mut stl_file).unwrap();
-
-    // Get starting point for finding bounding box
-    let t1 = stl_iter.next().unwrap().unwrap();
-    let v1 = t1.vertices[0];
-    let mut bounds = BoundingBox::new(&v1);
-
-    let mut face_count = 0;
-    let mut geometry = Geometry {
-        ..Geometry::default()
-    };
-
-    process_tri(&t1, &mut geometry, &mut bounds);
-    face_count += 1;
-
-    for triangle in stl_iter {
-        process_tri(&triangle.unwrap(), &mut geometry, &mut bounds);
-        face_count += 1;
-        //println!("{:?}",triangle);
-    }
-
-    println!("Bounds:");
-    println!("X: {}, {}", bounds.min.x, bounds.max.x);
-    println!("Y: {}, {}", bounds.min.y, bounds.max.y);
-    println!("Z: {}, {}", bounds.min.z, bounds.max.z);
-    println!("Center:");
-    println!("{:?}", bounds.center());
-    println!("Triangles processed:");
-    println!("{}", face_count);
-    println!();
-
-    Ok((geometry, bounds))
-}
-
-fn locate_camera(bounds: &BoundingBox) -> mint::Point3<f32> {
+fn locate_camera(bounds: &mesh::BoundingBox) -> mint::Point3<f32> {
     // Transform bounding box into camera space
     let p1 = bounds.max - bounds.center();
     let rot: cgmath::Basis2<f32> = cgmath::Rotation2::from_angle(cgmath::Deg(-CAM_AZIMUTH_DEG));
@@ -215,79 +76,79 @@ pub fn run(config: &Config) -> Result<(), Box<Error>> {
     // =========================
 
     let stl_file = File::open(&config.stl_filename)?;
-    let (geometry, bounds) = load_mesh(stl_file)?;
-    let center = bounds.center();
+    let mesh = Mesh::from_stl(stl_file)?;
+    let center = mesh.bounds.center();
 
     // Graphics Stuff
     // ==============
 
-    let mut window = three::Window::new(env!("CARGO_PKG_NAME"));
-    window.scene.background = three::Background::Color(0xFFFFFF);
+    //let mut window = three::Window::new(env!("CARGO_PKG_NAME"));
+    //window.scene.background = three::Background::Color(0xFFFFFF);
 
-    println!("== STL ==");
-    debug_geo(&geometry);
-    let material = three::material::Phong {
-        color: 0x00A0ff,
-        glossiness: 80.0,
-    };
-    let mesh = window.factory.mesh(geometry, material);
-    window.scene.add(&mesh);
+    //println!("== STL ==");
+    //mesh.debug();
+    //let material = three::material::Phong {
+    //    color: 0x00A0ff,
+    //    glossiness: 80.0,
+    //};
+    //let mesh = window.factory.mesh(geometry, material);
+    //window.scene.add(&mesh);
 
-    //let camera = window.factory.orthographic_camera(cam_center, yextent, zrange);
-    let camera = window.factory.perspective_camera(CAM_FOV_DEG, 1.0..500.0);
-    //let cam_pos = [150.0, -150.0, 150.0];
-    let cam_pos = locate_camera(&bounds);
-    camera.set_position(cam_pos);
-    camera.look_at(cam_pos, center, None);
+    ////let camera = window.factory.orthographic_camera(cam_center, yextent, zrange);
+    //let camera = window.factory.perspective_camera(CAM_FOV_DEG, 1.0..500.0);
+    ////let cam_pos = [150.0, -150.0, 150.0];
+    //let cam_pos = locate_camera(&bounds);
+    //camera.set_position(cam_pos);
+    //camera.look_at(cam_pos, center, None);
 
     // Plane
-    let plane = {
-        let geometry = Geometry::plane(
-            (bounds.max.x - bounds.min.x) * 3.0,
-            (bounds.max.y - bounds.min.y) * 3.0,
-        );
-        let material = three::material::Lambert {
-            //color: 0xA0ffA0,
-            color: 0xffffff,
-            flat: false,
-        };
-        window.factory.mesh(geometry, material)
-    };
-    plane.set_position([center[0], center[1], bounds.min.z]);
-    window.scene.add(&plane);
+    //let plane = {
+    //    let geometry = Geometry::plane(
+    //        (bounds.max.x - bounds.min.x) * 3.0,
+    //        (bounds.max.y - bounds.min.y) * 3.0,
+    //    );
+    //    let material = three::material::Lambert {
+    //        //color: 0xA0ffA0,
+    //        color: 0xffffff,
+    //        flat: false,
+    //    };
+    //    window.factory.mesh(geometry, material)
+    //};
+    //plane.set_position([center[0], center[1], bounds.min.z]);
+    //window.scene.add(&plane);
 
     // Test sphere
-    let sphere = {
-        let geometry = Geometry::uv_sphere(40.0, 20, 20);
-        println!("== Sphere ==");
-        debug_geo(&geometry);
-        let material = three::material::Phong {
-            color: 0xffA0A0,
-            glossiness: 80.0,
-        };
-        window.factory.mesh(geometry, material)
-    };
-    sphere.set_position([30.0, 40.0, 2.5]);
+    //let sphere = {
+    //    let geometry = Geometry::uv_sphere(40.0, 20, 20);
+    //    println!("== Sphere ==");
+    //    debug_geo(&geometry);
+    //    let material = three::material::Phong {
+    //        color: 0xffA0A0,
+    //        glossiness: 80.0,
+    //    };
+    //    window.factory.mesh(geometry, material)
+    //};
+    //sphere.set_position([30.0, 40.0, 2.5]);
     //window.scene.add(&sphere);
 
     // Lights
     //let hemisphere_light = window.factory.hemisphere_light(0xffffff, 0x8080ff, 0.5);
     //window.scene.add(&hemisphere_light);
     // TODO: Change dir_light position and shadow map size based on size of object
-    let mut dir_light = window.factory.directional_light(0xffffff, 0.9);
-    dir_light.look_at([-100.0, -400.0, 350.0], [0.0, 0.0, 0.0], None);
-    let shadow_map = window.factory.shadow_map(2048, 2048);
-    dir_light.set_shadow(shadow_map, 400.0, 1.0..1000.0);
-    window.scene.add(&dir_light);
-    let ambient_light = window.factory.ambient_light(0xdc8874, 0.5);
-    window.scene.add(ambient_light);
-    let point_light = window.factory.point_light(0xffffff, 0.5);
-    point_light.set_position([150.0, 350.0, 350.0]);
-    window.scene.add(point_light);
+    //let mut dir_light = window.factory.directional_light(0xffffff, 0.9);
+    //dir_light.look_at([-100.0, -400.0, 350.0], [0.0, 0.0, 0.0], None);
+    //let shadow_map = window.factory.shadow_map(2048, 2048);
+    //dir_light.set_shadow(shadow_map, 400.0, 1.0..1000.0);
+    //window.scene.add(&dir_light);
+    //let ambient_light = window.factory.ambient_light(0xdc8874, 0.5);
+    //window.scene.add(ambient_light);
+    //let point_light = window.factory.point_light(0xffffff, 0.5);
+    //point_light.set_position([150.0, 350.0, 350.0]);
+    //window.scene.add(point_light);
 
-    while window.update() {
-        window.render(&camera);
-    }
+    //while window.update() {
+    //    window.render(&camera);
+    //}
 
     Ok(())
 }
---
+++ src/mesh.rs
@@ -0,0 +1,174 @@
+extern crate cgmath;
+extern crate stl_io;
+
+use std::error::Error;
+use std::fs::File;
+use std::fmt;
+
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    position: stl_io::Vertex,
+    //texcoords: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+//implement_vertex!(Vertex, position, texcoords);
+
+#[derive(Copy, Clone)]
+pub struct Normal {
+    normal: stl_io::Normal,
+}
+
+implement_vertex!(Normal, normal);
+
+pub struct BoundingBox {
+    pub min: cgmath::Point3<f32>,
+    pub max: cgmath::Point3<f32>,
+}
+
+impl BoundingBox {
+    fn new(vert: &stl_io::Vertex) -> BoundingBox {
+        BoundingBox {
+            min: cgmath::Point3 {
+                x: vert[0],
+                y: vert[1],
+                z: vert[2],
+            },
+            max: cgmath::Point3 {
+                x: vert[0],
+                y: vert[1],
+                z: vert[2],
+            },
+        }
+    }
+    fn expand(&mut self, vert: &stl_io::Vertex) {
+        if vert[0] < self.min.x {
+            self.min.x = vert[0];
+        } else if vert[0] > self.max.x {
+            self.max.x = vert[0];
+        }
+        if vert[1] < self.min.y {
+            self.min.y = vert[1];
+        } else if vert[1] > self.max.y {
+            self.max.y = vert[1];
+        }
+        if vert[2] < self.min.z {
+            self.min.z = vert[2];
+        } else if vert[2] > self.max.z {
+            self.max.z = vert[2];
+        }
+    }
+    pub fn center(&self) -> cgmath::Point3<f32> {
+        cgmath::Point3 {
+            x: (self.min.x + self.max.x) / 2.0,
+            y: (self.min.y + self.max.y) / 2.0,
+            z: (self.min.z + self.max.z) / 2.0,
+        }
+    }
+}
+
+impl fmt::Display for BoundingBox {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "X: {}, {}", self.min.x, self.max.x)?;
+        writeln!(f, "Y: {}, {}", self.min.y, self.max.y)?;
+        writeln!(f, "Z: {}, {}", self.min.z, self.max.z)?;
+        Ok(())
+    }
+}
+
+
+pub struct Mesh {
+    pub vertices: Vec<Vertex>,
+    pub normals: Vec<Normal>,
+    pub indices: Vec<usize>,
+    pub bounds: BoundingBox,
+}
+
+impl Mesh {
+    pub fn from_stl(mut stl_file: File) -> Result<Mesh, Box<Error>> {
+        //let stl = stl_io::read_stl(&mut stl_file)?;
+        //println!("{:?}", stl);
+        let mut stl_iter = stl_io::create_stl_reader(&mut stl_file).unwrap();
+
+        // Get starting point for finding bounding box
+        let t1 = stl_iter.next().unwrap().unwrap();
+        let v1 = t1.vertices[0];
+
+        let mut mesh = Mesh {
+            vertices: Vec::new(),
+            normals: Vec::new(),
+            indices: Vec::new(),
+            bounds: BoundingBox::new(&v1),
+        };
+
+        let mut face_count = 0;
+        mesh.process_tri(&t1);
+        face_count += 1;
+
+        for triangle in stl_iter {
+            mesh.process_tri(&triangle.unwrap());
+            face_count += 1;
+            //println!("{:?}",triangle);
+        }
+
+        println!("Bounds:");
+        println!("{}",mesh.bounds);
+        println!("Center:");
+        println!("{:?}", mesh.bounds.center());
+        println!("Triangles processed:");
+        println!("{}", face_count);
+        println!();
+
+        Ok(mesh)
+    }
+
+    pub fn debug(&self) {
+        println!("Verts: {}", self.vertices.len());
+        println!("Norms: {}", self.normals.len());
+        //println!("Tex Coords: {:?}", geometry.tex_coords);
+        println!("Indices: {:?}", self.indices.len());
+        println!();
+    }
+
+    fn process_tri(&mut self, tri: &stl_io::Triangle) {
+        for v in tri.vertices.iter() {
+            self.bounds.expand(&v);
+            self.vertices.push( Vertex {
+                position: *v,
+            });
+            //println!("{:?}", v);
+        }
+        // Use normal from STL file if it is provided, otherwise calculate it ourselves
+        let n: stl_io::Normal;
+        if tri.normal == [0.0, 0.0, 0.0] {
+            println!("Calculating surface normal");
+            n = normal(&tri);
+        } else {
+            n = tri.normal;
+        }
+        //println!("{:?}",tri.normal);
+        for _ in 0..3 {
+            self.normals.push( Normal{
+                normal: n,
+            });
+        }
+    }
+}
+
+// Calculate surface normal of triangle using cross product
+// TODO: The GPU can probably do this a lot faster than we can.
+// See if there is an option for offloading this.
+fn normal(tri: &stl_io::Triangle) -> stl_io::Normal {
+    let p1: cgmath::Vector3<f32> = tri.vertices[0].into();
+    let p2: cgmath::Vector3<f32> = tri.vertices[1].into();
+    let p3: cgmath::Vector3<f32> = tri.vertices[2].into();
+    let v = p2 - p1;
+    let w = p3 - p1;
+    let n = v.cross(w);
+    let mag = n.x.abs() + n.y.abs() + n.z.abs();
+    [
+        n.x / mag,
+        n.y / mag,
+        n.z / mag,
+    ]
+}
