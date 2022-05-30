d05dcb264895de68f2797494b79ee88e8f83f3df
unlimitedbacon <unlimitedbacon@gmail.com>
2022-05-30 00:00:26 -0700

    Fix benchy bench resolution

--- benches/benchy.rs
+++ benches/benchy.rs
@@ -9,8 +9,8 @@ fn benchy() {
     let config = Config {
         stl_filename: "test_data/3DBenchy.stl".to_string(),
         img_filename: "benchy.png".to_string(),
-        width: 1920,
-        height: 1080,
+        width: 1024,
+        height: 768,
         ..Default::default()
     };
 
